46f2cee655700b9aedec68fdb95af6a6f51ac46b
Dannon Baker <dannon.baker@emory.edu>
2010-07-15 16:28:20 -0400

    Removed abs python path.  Removed unused dependencies.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export PATH=/opt/galaxy/bin:/opt/ec2/bin:/usr/gnu/bin:/usr/bin:/usr/X11/bin:/usr/sbin:/sbin:/bin:$PATH
 
 cd `dirname $0`
-/usr/bin/python ./scripts/paster.py serve cm_wsgi.ini --pid-file=cm_webapp.pid $@
+python ./scripts/paster.py serve cm_wsgi.ini --pid-file=cm_webapp.pid $@
