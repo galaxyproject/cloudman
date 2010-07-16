3403e10eaf35e0a72b6d2461170a635bf298b32b
Enis Afgan <afgane@gmail.com>
2010-07-16 11:38:01 -0400

    setup.sh will now create userData.txt file for test environment if the file does not exist

--- setup.sh
+++ setup.sh
@@ -15,3 +15,16 @@ for sample_file in $SAMPLE_FILES; do
         cp $sample_file $file
     fi
 done
+
+ud_file="userData.txt"
+if [ ! -f $ud_file ]; then
+    echo "User data file '$ud_file' not found; creating a test version of the file."
+    echo "CLUSTER_NAME=local test" > $ud_file
+    echo "AWS_ACCESS_KEY=<your AWS access key>" >> $ud_file
+    echo "AWS_PRIVATE_KEY=<your AWS private key>" >> $ud_file
+    echo "CM_HOME=`pwd`" >> $ud_file
+    echo "ROLE=master" >> $ud_file
+    echo "MASTER_IP=`hostname`" >> $ud_file
+    echo "BUCKET_NAME=TestBucket" >> $ud_file
+    echo "TESTFLAG=True" >> $ud_file
+fi
