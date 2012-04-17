359ec8a2149c5744ac400ad07699cc3f54cdec87
Gregory Long <gregorydavidlong@gmail.com>
2012-04-17 15:41:29 +1000

    Added entry to .hgignore to ignore swp files generated by vim.

---
+++ cm/base/controller.py
@@ -0,0 +1,11 @@
+"""Contains functionality needed in every webapp interface"""
+import os, time, logging
+from cm import util
+
+log = logging.getLogger( 'cloudman' )
+
+class BaseController( object ):
+    """Base class for CM webapp application controllers."""
+    def __init__( self, app ):
+        """Initialize an interface for application 'app'"""
+        self.app = app
---
+++ cm/services/data/__init__.py
@@ -0,0 +1,10 @@
+"""
+Placeholder for DataService methods.
+"""
+
+from cm.services import Service
+
+class DataService( Service ):
+    
+    def __init__(self, app):
+        super(DataService, self).__init__(app)
