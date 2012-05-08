2b992fb38de45e1340996b05942c7d81db97cf5e
Enis Afgan <afgane@gmail.com>
2012-05-08 14:16:28 +1000

    Ensure Galaxy's universe_wsgi.ini is owned by galaxy user after adding admin users

---
+++ cm/services/__init__.py
@@ -0,0 +1,65 @@
+"""Services Package"""
+from cm.util.bunch import Bunch
+import datetime as dt
+
+import logging
+log = logging.getLogger( 'cloudman' )
+
+
+service_states = Bunch(
+    UNSTARTED="Unstarted",
+    WAITING_FOR_USER_ACTION="Waiting for user action",
+    STARTING="Starting",
+    RUNNING = "Running",
+    SHUTTING_DOWN = "Shutting down",
+    SHUT_DOWN="Shut down",
+    ERROR="Error"
+ )
+
+
+class Service( object):
+    
+    def __init__( self, app, service_type=None ):
+        self.app = app
+        self.state = service_states.UNSTARTED
+        self.last_state_change_time = dt.datetime.utcnow()
+        self.svc_type = "BaseService"
+        self.reqs = {}
+
+    def add (self):
+        if self.state != service_states.RUNNING:
+            log.debug("Trying to add service '%s'" % self.svc_type)
+            self.state = service_states.STARTING
+            self.last_state_change_time = dt.datetime.utcnow()
+            flag = True # indicate if current service prerequisites are satisfied
+            for svc_type, svc_name in self.reqs.iteritems():
+                log.debug("'%s' service checking its prerequisite '%s:%s'" \
+                    % (self.get_full_name(), svc_type, svc_name))
+                for svc in self.app.manager.services:
+                    # log.debug("Checking service %s state." % svc.svc_type)
+                    if svc_type==svc.svc_type:
+                        if svc_name is not None and svc.name==svc_name:
+                            # log.debug("Service %s:%s running: %s" % (svc.svc_type, svc.name, svc.running()))
+                            if not svc.running():
+                                flag = False
+                        else:
+                            # log.debug("Service %s running: %s" % (svc.svc_type, svc.running()))
+                            if not svc.running():
+                                flag = False
+            if flag:
+                log.info("{0} prerequisites OK; starting the service".format(self.get_full_name()))
+                self.start()
+                return True
+            else:
+                log.info("{0} service prerequisites are not yet satisfied; setting service state to {1}"\
+                    .format(self.get_full_name(), service_states.UNSTARTED))
+                # Reset state so it get picked back up by monitor
+                self.state = service_states.UNSTARTED
+                return False
+    
+    def running(self):
+        return self.state == service_states.RUNNING
+        
+    def get_full_name(self):
+        """ Return full name of the service (useful if different from service type)"""
+        return self.svc_type
---
+++ templates/srvc_log.mako
@@ -0,0 +1,21 @@
+<%inherit file="/base_panels.mako"/>
+<%def name="main_body()">
+    <div id='msg_warning'>
+        %if not full:
+            Only up to the most recent ${num_lines} lines of the file (${log_file})
+            are shown. 
+            <a href="?service_name=${service_name}&show=all">Show all</a>
+            %if num_lines > 100:
+                | <a href="?service_name=${service_name}&show=less&num_lines=${num_lines}">Show less</a>
+            %endif
+            | <a href="?service_name=${service_name}&show=more&num_lines=${num_lines}">Show more</a>
+        %else:
+            The entire log file (${log_file}) is shown.
+            <a href="?service_name=${service_name}&show=latest">Show latest</a>
+        %endif
+        | <a href="${h.url_for(controller='root', action='admin')}">Back to admin view</a>
+    </div>
+    <div class="srvc_log">
+        <pre>${log}</pre>
+    </div>
+</%def>
