4bcd1e6de9128bbc785082abd075979e819cda34
Enis Afgan <afgane@gmail.com>
2012-10-18 16:41:13 +1100

    Fix field access call in bucket.py

--- cm/services/data/bucket.py
+++ cm/services/data/bucket.py
@@ -67,7 +67,7 @@ class Bucket(object):
         """
         details['DoT']      = "No"
         details['bucket_name'] = self.bucket_name
-        details['access_key'] = self.access_key
+        details['access_key'] = self.a_key
         # TODO: keep track of any errors
         details['err_msg']  = None if details.get('err_msg', '') == '' else details['err_msg']
         return details
