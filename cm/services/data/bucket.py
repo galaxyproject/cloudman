772dbc82a3913c9f6d02fc079f610fee8be1f0e8
Enis Afgan <afgane@gmail.com>
2012-11-21 16:03:26 +1100

    Fix a race condition when adding a bucket as a file system but first need to install s3fs

--- cm/services/data/bucket.py
+++ cm/services/data/bucket.py
@@ -3,8 +3,10 @@ Methods for managing S3 Buckets and making them available as local file systems.
 """
 import os
 import urllib2
+import threading
 
 from cm.util import misc
+from cm.services import service_states
 from cm.util.misc import _if_not_installed
 from cm.util.decorators import TestFlag
 
@@ -38,19 +40,31 @@ class Bucket(object):
             self.s_key = s_key
         log.debug("If needed, when mounting bucket {0} will use the following credentials: '{1}' & '{2}'"\
             .format(self.bucket_name, self.a_key, self.s_key))
-        self._install_s3fs()
+        # Before we can mount a bucket, s3fs needs to be installed; installing
+        # s3fs takes a while and it thus done in a separate thread not to block.
+        # However, regular status updates will kick in before s3fs is installed
+        # so keep up with the installation progress to be able to appropriately
+        # update status. Because the install runs in a separate thread, without
+        # much work (see Python's Queue.Queue), we'll just set the flag as if
+        # s3fs is installed by default and update the value directly from the
+        # method vs. via a return value.
+        self.s3fs_installed = True
+        threading.Thread(target=self._install_s3fs).start()
 
     @TestFlag(True)
     @_if_not_installed("s3fs")
     def _install_s3fs(self):
         msg = "s3fs is not installed; will install it now (this typically takes 2-5 minutes)."
+        self.s3fs_installed = False
         log.info(msg)
         self.app.msgs.info(msg)
         misc.run("cd /tmp;wget --output-document=s3fs.sh http://s3.amazonaws.com/cloudman/pss/s3fs.sh")
         if misc.run("cd /tmp;bash s3fs.sh"):
             msg = "Done installing s3fs"
+            self.s3fs_installed = True
         else:
             msg = "Trouble installing sf3s; giving up."
+            self.fs.state = service_states.ERROR
         log.debug(msg)
         self.app.msgs.info(msg)
 
@@ -106,6 +120,11 @@ class Bucket(object):
         Mount the bucket as a local file system, making it available at
         ``self.fs.mount_point`` (which is typically ``/mnt/filesystem_name``)
         """
+        if not self.s3fs_installed:
+            log.debug("Waiting for s3fs to install before mounting bucket {0}"\
+                .format(self.bucket_name))
+            self.fs.state = service_states.UNSTARTED
+            return True
         try:
             log.debug("Mounting file system {0} from bucket {1} to {2}"\
                 .format(self.fs.get_full_name(), self.bucket_name, self.mount_point))
