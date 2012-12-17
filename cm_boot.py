7a67db6edde5ed7a119c0d55d088e15001190150
nuwan_ag <nuwan.goonasekera@gmail.com>
2012-12-17 17:02:27 +1100

    Added optional Loggly based cloud logging support (www.loggly.com)

--- cm_boot.py
+++ cm_boot.py
@@ -374,6 +374,7 @@ def main():
     _run('easy_install oca') # temp only - this needs to be included in the AMI (incl. in CBL AMI!)
     _run('easy_install Mako==0.7.0') # required for Galaxy Cloud AMI ami-da58aab3
     _run('easy_install boto==2.6.0') # required for older AMIs
+    _run('easy_install hoover') # required for older AMIs
     with open(os.path.join(CM_BOOT_PATH, USER_DATA_FILE)) as ud_file:
         ud = yaml.load(ud_file)
     if len(sys.argv) > 1:
