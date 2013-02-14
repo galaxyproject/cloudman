7b99b92904a27b031c6ab2dd88f9d66bb2d1597b
nuwan_ag <nuwan.goonasekera@gmail.com>
2013-02-15 10:46:12 +1100

    Added UI for viewing service details

---
+++ cm/framework/messages/constants.py
@@ -0,0 +1,15 @@
+DEBUG = 10
+INFO = 20
+SUCCESS = 25
+WARNING = 30
+ERROR = 40
+CRITICAL = 50
+
+DEFAULT_TAGS = {
+    DEBUG: 'debug',
+    INFO: 'info',
+    SUCCESS: 'success',
+    WARNING: 'warning',
+    ERROR: 'error',
+    CRITICAL: 'critical',
+}
