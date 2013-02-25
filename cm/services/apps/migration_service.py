f36b4a05a4c3da926043ccd0ea4f9e54c3517746
Brad Chapman <chapmanb@50mail.com>
2013-02-25 11:33:56 -0500

    Postgres upgrade from 8.4 to 9.1 databases and migration to new database location

--- cm/services/apps/migration_service.py
+++ cm/services/apps/migration_service.py
@@ -4,12 +4,46 @@ from cm.services import service_states
 from cm.services import ServiceDependency
 from cm.services import ServiceType
 from cm.services.apps import ApplicationService
+from cm.util import misc, paths
 
 import logging
 log = logging.getLogger('cloudman')
 
+class Migrate1to2:
+    """Functionality for upgrading from version 1 to 2.
+    """
+    def _as_postgres(self, cmd):
+        return misc.run('%s - postgres -c "%s"' % (paths.P_SU, cmd))
 
-class MigrationService(ApplicationService):
+    def _upgrade_postgres_8_to_9(self, app):
+        data_dir = os.path.join(app.path_resolver.galaxy_data, "pgsql", "data")
+        if os.path.exists(data_dir):
+            with open(os.path.join(data_dir, "PG_VERSION")) as in_handle:
+                version = in_handle.read().strip()
+            if version.startswith("8"):
+                log.info("Upgrading Postgres from version 8 to 9")
+                old_debs = ["http://launchpadlibrarian.net/102366400/postgresql-client-8.4_8.4.11-1_amd64.deb",
+                            "http://launchpadlibrarian.net/102366396/postgresql-8.4_8.4.11-1_amd64.deb"]
+                for deb in old_debs:
+                    misc.run("wget %s" % deb)
+                    misc.run("dpkg -i %s" % os.path.basename(deb))
+                    misc.run("rm -f %s" % os.path.basename(deb))
+                backup_dir = os.path.join(app.path_resolver.galaxy_data, "pgsql",
+                                          "data-backup-v%s" % version)
+                self._as_postgres("mv %s %s" % (data_dir, backup_dir))
+                self._as_postgres("%s/initdb %s" % (app.path_resolver.pg_home, data_dir))
+                self._as_postgres("pg_createcluster -d %s %s old_galaxy" % (backup_dir, version))
+                self._as_postgres("pg_upgradecluster %s old_galaxy %s" % (version, data_dir))
+                misc.run("apt-get remove postgresql-8.4 postgresql-client-8.4")
+
+    def _move_postgres_location(self, app):
+        old_dir = os.path.join(app.path_resolver.galaxy_data, "pgsql")
+        if os.path.exists(old_dir) and not os.path.exists(app.path_resolver.psql_dir):
+            log.info("Moving Postgres location from %s to %s" %
+                     (old_dir, app.path_resolver.psql_dir))
+            misc.run("mv %s %s" % (old_dir, app.path_resolver.psql_dir))
+
+class MigrationService(ApplicationService, Migrate1to2):
     def __init__(self, app):
         super(MigrationService, self).__init__(app)
         self.svc_roles = [ServiceRole.MIGRATION]
@@ -56,8 +90,8 @@ class MigrationService(ApplicationService):
 
     def migrate_1(self):
         log.debug("Migrating from version 1 to 2...")
-        # mount file systems from persistent_data.yaml
-        # Upgrade DB
+        self._upgrade_postgres_8_to_9(self.app)
+        self._move_postgres_location(self.app)
         # copy tools FS to the data FS
         # adjust directory names/paths to match the new FS structure
         # sed for predefined full old paths (eg, Galaxy's env.sh files, EMBOSS tools?)
