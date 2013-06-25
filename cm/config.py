c3f1561d516e5427edf3c66e8874ffad35ab865d
John Chilton <jmchilton@gmail.com>
2013-06-25 08:32:43 -0500

    Add ability to setup extra SSH authorized keys in user data. Specify these keys as a user data list property 'authorized_keys'. Defaults to configuring these for ubuntu and galaxy user, though this can be configured with the user data option 'authorized_key_users'. Place new functionality in cm/boot/conf.py module as well. Move handling of nginx configuration and 'conf_files' user data property into this module.
    Implement tests for this new functionality as well as for functionality moved into new cm.boot.conf module ('conf_files' and nginx configuration).

---
+++ cm/config.py
@@ -0,0 +1,229 @@
+"""Universe configuration builder."""
+import time
+import sys
+import os
+import logging
+import logging.config
+import ConfigParser
+# from optparse import OptionParser
+from cm.util import string_as_bool
+from cm.util import misc
+from cm.util import paths
+import hoover
+
+log = logging.getLogger('cloudman')
+
+DEFAULT_INSTANCE_REBOOT_TIMEOUT = 300
+DEFAULT_INSTANCE_COMM_TIMEOUT = 180
+DEFAULT_INSTANCE_STATE_CHANGE_WAIT = 400
+DEFAULT_INSTANCE_REBOOT_ATTEMPTS = 4
+DEFAULT_INSTANCE_TERMINATE_ATTEMPTS = 4
+DEFAULT_INSTANCE_TYPES = [
+    ("", "Same as Master"),
+    ("t1.micro", "Micro"),
+    ("m1.small", "Small"),
+    ("m1.medium", "Medium"),
+    ("m1.large", "Large"),
+    ("m1.xlarge", "Extra Large"),
+    ("m2.xlarge", "High-Memory Extra Large"),
+    ("m2.2xlarge", "High-Memory Double Extra Large"),
+    ("m2.4xlarge", "High-Memory Quadruple Extra Large"),
+    ("c1.xlarge", "High-CPU Extra Large"),
+]
+
+
+def resolve_path(path, root):
+    """If 'path' is relative make absolute by prepending 'root'"""
+    if not(os.path.isabs(path)):
+        path = os.path.join(root, path)
+    return path
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+class Configuration(object):
+    def __init__(self, **kwargs):
+        self.config_dict = kwargs
+        self.root = kwargs.get('root_dir', '.')
+        # Where dataset files are stored
+        self.id_secret = kwargs.get(
+            "id_secret", "USING THE DEFAULT IS NOT SECURE!")
+        self.use_remote_user = string_as_bool(
+            kwargs.get("use_remote_user", "False"))
+        self.require_login = string_as_bool(
+            kwargs.get("require_login", "False"))
+        self.template_path = resolve_path(
+            kwargs.get("template_path", "templates"), self.root)
+        self.cloudman_source_file_name = kwargs.get(
+            "cloudman_file_name", "cm.tar.gz")
+        # self.template_cache = resolve_path( kwargs.get(
+        # "template_cache_path", "database/reports/compiled_templates" ),
+        # self.root )
+        self.sendmail_path = kwargs.get('sendmail_path', "/usr/sbin/sendmail")
+        self.brand = kwargs.get('brand', None)
+        self.wiki_url = kwargs.get('wiki_url', "http://g2.trac.bx.psu.edu/")
+        self.GC_url = kwargs.get(
+            'GC_url', 'http://bitbucket.org/afgane/galaxy-central-gc2/')
+        self.CM_url = kwargs.get(
+            'GC_url', 'http://bitbucket.org/galaxy/cloudman/')
+        self.bugs_email = kwargs.get(
+            'bugs_email', "mailto:galaxy-bugs@bx.psu.edu")
+        self.blog_url = kwargs.get(
+            'blog_url', "http://g2.trac.bx.psu.edu/blog")
+        self.screencasts_url = kwargs.get(
+            'screencasts_url', "http://g2.trac.bx.psu.edu/wiki/ScreenCasts")
+        # Parse global_conf
+        global_conf = kwargs.get('global_conf', None)
+        global_conf_parser = ConfigParser.ConfigParser()
+        if global_conf and "__file__" in global_conf:
+            global_conf_parser.read(global_conf['__file__'])
+        # Load supported image configuration from a file on the image.
+        # This is a dict containing a list of configurations supported by
+        # the by system/image. Primarily, this contains a list
+        # of apps (available under key 'apps'). This config file allows CloudMan
+        # to integrate native and custom support for additional applications.
+        # The dict contains default values for backward combatibility.
+        self.ic = {'apps': ['cloudman', 'galaxy']}
+        if os.path.exists(paths.IMAGE_CONF_SUPPORT_FILE):
+            self.ic = misc.load_yaml_file(paths.IMAGE_CONF_SUPPORT_FILE)
+        # Logger is not configured yet so print
+        print "Image configuration suports: %s" % self.ic
+        self.instance_types = []
+
+    def get(self, key, default):
+        return self.config_dict.get(key, default)
+
+    def check(self):
+        # Check that required directories exist
+        for path in self.root, self.template_path:
+            if not os.path.isdir(path):
+                raise ConfigurationError(
+                    "Directory does not exist: %s" % path)
+
+    def init_with_user_data(self, user_data):
+        self.__configure_instance_management(user_data)
+        self.__configure_instance_types(user_data)
+        self.condor_enabled = user_data.get("condor_enabled", True)
+        self.hadoop_enabled = user_data.get("hadoop_enabled", True)
+
+    def __configure_instance_management(self, user_data):
+        """Configure attributes used control reboot/terminate behavior
+        of cm.util.master:Instance."""
+        self.instance_reboot_timeout = \
+            user_data.get("instance_reboot_timeout", DEFAULT_INSTANCE_REBOOT_TIMEOUT)
+        self.instance_comm_timeout = \
+            user_data.get("instance_comm_timeout", DEFAULT_INSTANCE_COMM_TIMEOUT)
+        self.instance_state_change_wait = \
+            user_data.get("instance_state_change_wait", DEFAULT_INSTANCE_STATE_CHANGE_WAIT)
+        self.instance_reboot_attempts = \
+            user_data.get("instance_reboot_attempts", DEFAULT_INSTANCE_REBOOT_ATTEMPTS)
+        self.instance_terminate_attempts = \
+            user_data.get("instance_terminate_attempts", DEFAULT_INSTANCE_TERMINATE_ATTEMPTS)
+
+    def __configure_instance_types(self, user_data):
+        cloud_name = user_data.get('cloud_name', 'amazon').lower()
+        if "instance_types" in user_data:
+            ## Manually specified instance types
+            user_data_instance_types = user_data["instance_types"]
+            instance_types = [(type_def["key"], type_def["name"]) for type_def in user_data_instance_types]
+        elif cloud_name == "nectar":
+            instance_types = [
+                ("", "Same as Master"),
+                ("m1.small", "Small"),
+                ("m1.medium", "Medium"),
+                ("m1.xlarge", "Extra Large"),
+                ("m1.xxlarge", "Extra Extra Large")
+            ]
+        elif cloud_name == "hpcloud":
+            instance_types = [
+                ("", "Same as Master"),
+                ("standard.xsmall", "Extra Small"),
+                ("standard.small", "Small"),
+                ("standard.medium", "Medium"),
+                ("standard.large", "Large"),
+                ("standard.xlarge", "Extra Large"),
+                ("standard.2xlarge", "Extra Extra Large"),
+            ]
+        elif cloud_name == "ict-tas":
+            instance_types = [
+                ("", "Same as Master"),
+                ("m1.small", "Small"),
+                ("m1.medium", "Medium"),
+                ("m1.xlarge", "Extra Large"),
+                ("m1.xxlarge", "Extra Extra Large"),
+            ]
+        else:
+            instance_types = DEFAULT_INSTANCE_TYPES
+        self.instance_types = instance_types
+
+
+def get_database_engine_options(kwargs):
+    """
+    Allow options for the SQLAlchemy database engine to be passed by using
+    the prefix "database_engine_option_".
+    """
+    conversions = {
+        'convert_unicode': string_as_bool,
+        'pool_timeout': int,
+        'echo': string_as_bool,
+        'echo_pool': string_as_bool,
+        'pool_recycle': int,
+        'pool_size': int,
+        'max_overflow': int,
+        'pool_threadlocal': string_as_bool
+    }
+    prefix = "database_engine_option_"
+    prefix_len = len(prefix)
+    rval = {}
+    for key, value in kwargs.iteritems():
+        if key.startswith(prefix):
+            key = key[prefix_len:]
+            if key in conversions:
+                value = conversions[key](value)
+            rval[key] = value
+    return rval
+
+
+def configure_logging(config, user_data={}):
+    """
+    Allow some basic logging configuration to be read from the cherrpy
+    config.
+    """
+    # format = config.get( "log_format", "%(name)s %(levelname)s %(asctime)s
+    # %(message)s" )
+    format = config.get(
+        "log_format", "%(asctime)s %(levelname)-7s %(module)12s:%(lineno)-4d %(message)s")
+    level = logging._levelNames[config.get("log_level", "DEBUG")]
+    destination = config.get("log_destination", "stdout")
+    log.info("Logging at '%s' level to '%s'" % (level, destination))
+    # Get root logger
+    root = logging.getLogger()
+    # Set level
+    root.setLevel(level)
+    # Turn down paste httpserver logging
+    if level <= logging.DEBUG:
+        logging.getLogger(
+            "paste.httpserver.ThreadPool").setLevel(logging.WARN)
+    # Remove old handlers
+    for h in root.handlers[:]:
+        root.removeHandler(h)
+    # Create handler
+    if destination == "stdout":
+        handler = logging.StreamHandler(sys.stdout)
+    else:
+        handler = logging.FileHandler(destination)
+    # Create formatter
+    formatter = logging.Formatter(format)
+    # Hook everything up
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+    # Add loggly handler
+    loggly_token = config.get('cm_loggly_token', None)
+    loggly_token = os.environ.get('CM_LOGGLY_TOKEN', loggly_token)
+    loggly_token = user_data.get('cm_loggly_token', loggly_token)
+    if loggly_token is not None:
+        loggly_handler = hoover.LogglyHttpHandler(token=loggly_token)
+        loggly_handler.setFormatter(formatter)
+        log.addHandler(loggly_handler)
---
+++ cm/util/galaxy_conf.py
@@ -0,0 +1,246 @@
+from os.path import join, exists
+from os import makedirs, symlink, chown
+from shutil import copyfile, move
+
+from ConfigParser import SafeConfigParser
+from pwd import getpwnam
+from grp import getgrnam
+
+from .misc import run
+from cm.util import paths
+
+import logging
+log = logging.getLogger('cloudman')
+
+
+OPTIONS_FILE_NAME = 'universe_wsgi.ini'
+
+
+def attempt_chown_galaxy_if_exists(path):
+    """
+    Change owner of file at specified `path` (if it exists)
+    to `galaxy`.
+    """
+    if exists(path):
+        attempt_chown_galaxy(path)
+
+
+def attempt_chown_galaxy(path):
+    """
+    Change owner of file at specified `path` to `galaxy`.
+    """
+    try:
+        galaxy_uid = getpwnam("galaxy")[2]
+        galaxy_gid = getgrnam("galaxy")[2]
+        chown(path, galaxy_uid, galaxy_gid)
+    except BaseException:
+        run("chown galaxy:galaxy '%s'" % path)
+
+
+def populate_admin_users(option_manager, admins_list=[]):
+    """ Galaxy admin users can now be added by providing them in user data
+        (see below) or by calling this method and providing a user list.
+        YAML format for user data for providing admin users
+        (note that these users will still have to manually register
+        on the given cloud instance):
+        admin_users:
+         - user@example.com
+         - user2@anotherexample.edu """
+    for admin in option_manager.app.ud.get('admin_users', []):
+        if admin not in admins_list:
+                admins_list.append(admin)
+    if len(admins_list) == 0:
+        return False
+    log.info('Adding Galaxy admin users: %s' % admins_list)
+    option_manager.set_properties({"admin_users": ",".join(admins_list)})
+
+
+def populate_dynamic_options(option_manager):
+    """
+    Use `option_manager` to populate arbitrary app:main and galaxy:tool_runners
+    properties coming in from userdata.
+    """
+    dynamic_option_types = {"galaxy_universe_": "app:main",
+                            "galaxy_tool_runner_": "galaxy:tool_runners",
+                            }
+    for option_prefix, section in dynamic_option_types.iteritems():
+        for key, value in option_manager.app.ud.iteritems():
+            if key.startswith(option_prefix):
+                key = key[len(option_prefix):]
+                option_manager.set_properties({key: value}, section=section)
+
+
+## High-level functions that utilize option_manager interface (defined below)
+## to configure Galaxy's options.
+def populate_process_options(option_manager):
+    """
+    Use `option_manager` to populate process (handler, manager, web) sections
+    for Galaxy.
+    """
+    app = option_manager.app
+    web_thread_count = int(app.ud.get("web_thread_count", 1))
+    handler_thread_count = int(app.ud.get("handler_thread_count", 1))
+    # Setup web threads
+    [__add_server_process(option_manager, i, "web", 8080)
+        for i in range(web_thread_count)]
+    # Setup handler threads
+    handlers = [__add_server_process(option_manager, i, "handler", 9080)
+        for i in range(handler_thread_count)]
+    # Setup manager thread
+    __add_server_process(option_manager, 0, "manager", 8079)
+    process_properties = {"job_manager": "manager0",
+                          "job_handlers": ",".join(handlers)}
+    option_manager.set_properties(process_properties)
+
+
+def __add_server_process(option_manager, index, prefix, initial_port):
+    app = option_manager.app
+    port = initial_port + index
+    threads = app.ud.get("threadpool_workers", "7")
+    server_options = {"use": "egg:Paste#http",
+                      "port": port,
+                      "use_threadpool": True,
+                      "threadpool_workers": threads
+                      }
+    server_name = "%s%d" % (prefix, index)
+    if port == 8080:
+        # Special case, server on port 8080 must be called main unless we want
+        # to start deleting chunks of universe_wsgi.ini.
+        server_name = "main"
+    option_manager.set_properties(server_options,
+                                  section="server:%s" % server_name,
+                                  description="server_%s" % server_name)
+    return server_name
+
+
+## Abstraction for interacting with Galaxy's options
+def galaxy_option_manager(app):
+    """ Returns a high-level class for managing Galaxy options.
+    """
+    ud = app.ud
+    if "galaxy_conf_dir" in ud:
+        option_manager = DirectoryGalaxyOptionManager(app)
+    else:
+        option_manager = FileGalaxyOptionManager(app)
+    return option_manager
+
+
+def populate_galaxy_paths(option_manager):
+    """
+    Turn ``path_resolver`` paths and configurations into Galaxy options using
+    specified ``option_manager``.
+    """
+    properties = {}
+    path_resolver = option_manager.app.path_resolver
+    properties["database_connection"] = "postgres://galaxy@localhost:{0}/galaxy"\
+        .format(paths.C_PSQL_PORT)
+    properties["genome_data_path"] = \
+        join(path_resolver.galaxy_indices, "genomes")
+    properties["len_file_path"] = \
+        join(path_resolver.galaxy_data, "configuration_data", "len")
+    properties["tool_dependency_dir"] = \
+        join(path_resolver.galaxy_tools, "tools")
+    properties["file_path"] = join(path_resolver.galaxy_data, "files")
+    temp_dir = join(path_resolver.galaxy_data, "tmp")
+    properties["new_file_path"] = temp_dir
+    # This is something a user may change so this is not an ideal solution
+    # but a relation to the required files is necessary so here it is.
+    properties['tool_config_file'] = "tool_conf.xml,shed_tool_conf.xml"
+    properties["job_working_directory"] = \
+        join(temp_dir, "job_working_directory")
+    properties["cluster_files_directory"] = \
+        join(temp_dir, "pbs")
+    properties["ftp_upload_dir"] = \
+        join(temp_dir, "ftp")
+    properties["library_import_dir"] = \
+        join(temp_dir, "library_import_dir")
+    properties["nginx_upload_store"] = \
+        join(path_resolver.galaxy_data, "upload_store")
+    # Allow user data options to override these, spefically database.
+    priority_offset = -1
+    option_manager.set_properties(properties, description="paths", priority_offset=priority_offset)
+
+
+class FileGalaxyOptionManager(object):
+    """
+    Default Galaxy option manager, modifies $galaxy_home/universe_wsgi
+    directly.
+    """
+
+    def __init__(self, app):
+        self.app = app
+
+    def setup(self):
+        """ setup should return conf_dir, in this case there is none."""
+        return None
+
+    def set_properties(self, properties, section="app:main", description=None, priority_offset=0):
+        galaxy_home = self.app.path_resolver.galaxy_home
+        config_file_path = join(galaxy_home, OPTIONS_FILE_NAME)
+        input_config_file_path = config_file_path
+        if not exists(input_config_file_path):
+            input_config_file_path = "%s.sample" % config_file_path
+        parser = SafeConfigParser()
+        configfile = open(input_config_file_path, 'rt')
+        parser.readfp(configfile)
+        for key, value in properties.iteritems():
+            parser.set(section, key, value)
+        configfile.close()
+        new_config_file_path = join(galaxy_home, 'universe_wsgi.ini.new')
+        with open(new_config_file_path, 'wt') as output_file:
+                parser.write(output_file)
+        move(new_config_file_path, config_file_path)
+        attempt_chown_galaxy(config_file_path)
+
+
+class DirectoryGalaxyOptionManager(object):
+    """
+    When `galaxy_conf_dir` in specified in UserData this is used to
+    manage Galaxy's options.
+    """
+
+    def __init__(self, app, conf_dir=None, conf_file_name=OPTIONS_FILE_NAME):
+        self.app = app
+        if not conf_dir:
+            conf_dir = app.ud["galaxy_conf_dir"]
+        self.conf_dir = conf_dir
+        self.conf_file_name = conf_file_name
+
+    def setup(self):
+        """ Setup the configuration directory and return conf_dir. """
+        self.__initialize_galaxy_config_dir()
+        return self.conf_dir
+
+    def __initialize_galaxy_config_dir(self):
+        conf_dir = self.conf_dir
+        if not exists(conf_dir):
+            makedirs(conf_dir)
+            defaults_destination = join(conf_dir, "010_%s" % self.conf_file_name)
+            galaxy_home = self.app.path_resolver.galaxy_home
+            universe_wsgi = join(galaxy_home, self.conf_file_name)
+            if not exists(universe_wsgi):
+                # Fresh install, take the oppertunity to just link in defaults
+                sample_name = "%s.sample" % self.conf_file_name
+                defaults_source = join(galaxy_home, sample_name)
+                symlink(defaults_source, defaults_destination)
+            else:
+                # CloudMan has previously been run without the galaxy_conf_dir
+                # option enabled. Users may have made modifications to
+                # universe_wsgi.ini that I guess we should preserve for
+                # backward compatibility.
+                defaults_source = join(galaxy_home, self.conf_file_name)
+                copyfile(defaults_source, defaults_destination)
+
+    def set_properties(self, properties, section="app:main", description=None, priority_offset=0):
+        if not properties:
+            return
+
+        priority = int(self.app.ud.get("galaxy_option_priority", "400")) + priority_offset
+        conf_dir = self.conf_dir
+        if description is None:
+            description = properties.keys()[0]
+        conf_file_name = "%s_cloudman_override_%s.ini" % (str(priority), description)
+        conf_file = join(conf_dir, conf_file_name)
+        props_str = "\n".join(
+            ["%s=%s" % (k, v) for k, v in properties.iteritems()])
+        open(conf_file, "w").write("[%s]\n%s" % (section, props_str))
