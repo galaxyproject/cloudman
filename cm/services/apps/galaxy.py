c476ea5ce12609c79b3ef5f50a40fcaf81449564
Enis Afgan <afgane@gmail.com>
2013-07-17 17:18:35 +0200

    Add TMPDIR env var to point to a dir on (galaxy's) data file system

--- cm/services/apps/galaxy.py
+++ cm/services/apps/galaxy.py
@@ -84,6 +84,7 @@ class GalaxyService(ApplicationService):
         log.debug("Using Galaxy from '{0}'".format(self.galaxy_home))
         os.putenv("GALAXY_HOME", self.galaxy_home)
         os.putenv("TEMP", self.app.path_resolver.galaxy_temp)
+        os.putenv("TMPDIR", self.app.path_resolver.galaxy_temp)
         conf_dir = self.option_manager.setup()
         if conf_dir:
             self.env_vars["GALAXY_UNIVERSE_CONFIG_DIR"] = conf_dir
