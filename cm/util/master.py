64b4aed7621a26717dae10df7b25fb35325e7581
Dannon Baker <dannonbaker@me.com>
2013-08-01 14:00:22 -0400

    Fix debug print statement.

--- cm/util/master.py
+++ cm/util/master.py
@@ -1291,7 +1291,7 @@ class ConsoleManager(BaseConsoleManager):
                 shared_cluster_pd_file):
             scpd = misc.load_yaml_file(shared_cluster_pd_file)
             self.initial_cluster_type = scpd.get('cluster_type', None)
-            log.debug("Initializing %s cluster type from shared cluster")
+            log.debug("Initializing %s cluster type from shared cluster" % self.initial_cluster_type)
             if 'shared_data_snaps' in scpd:
                 shared_data_vol_snaps = scpd['shared_data_snaps']
                 try:
