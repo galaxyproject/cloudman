510b967032668b4f108b5971c2bf3bc4a2a00193
nuwan_ag <nuwan_ag@localhost>
2014-05-05 18:23:01 +1000

    Fixed incorrect parameter placement.

--- cm/util/worker.py
+++ cm/util/worker.py
@@ -175,7 +175,7 @@ class ConsoleManager(BaseConsoleManager):
                     # TODO use the actual filesystem name for accounting/status
                     # updates
                     mount_points.append(
-                        (mp['fs_name'], mp['shared_mount_path'], mp['fs_type'], mp['server']), mp.get('mount_options', None))
+                        (mp['fs_name'], mp['shared_mount_path'], mp['fs_type'], mp['server'], mp.get('mount_options', None)))
             else:
                 raise Exception("Mount point parsing failure.")
         except Exception, e:
