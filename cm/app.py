293d10f043e3fb5be5dbe437f9c10aea58abe371
Dannon Baker <dannon.baker@gmail.com>
2014-08-01 13:50:48 -0400

    Cleanup prior to merging slurm and refactoring work.

---
+++ cm/app.py
@@ -0,0 +1,123 @@
+import config
+import logging
+import logging.config
+import sys
+from cm.util import misc
+from cm.util import paths
+from cm.framework import messages
+from cm.clouds.cloud_config import CloudConfig
+
+log = logging.getLogger('cloudman')
+logging.getLogger('boto').setLevel(logging.INFO)
+
+
+class CMLogHandler(logging.Handler):
+    def __init__(self, app):
+        logging.Handler.__init__(self)
+        self.formatter = logging.Formatter(
+            "%(asctime)s - %(message)s", "%H:%M:%S")
+        # self.formatter = logging.Formatter("[%(levelname)s]
+        # %(module)s:%(lineno)d %(asctime)s: %(message)s")
+        self.setFormatter(self.formatter)
+        # Limit the size of the log message buffer to 1000 lines. This log is
+        # used on the UI and causes responsivness issues once the log grows
+        self.log_buffer = misc.RingBuffer(1000)
+
+    @property
+    def logmessages(self):
+        return self.log_buffer.tolist()
+
+    def emit(self, record):
+        self.log_buffer.append(self.formatter.format(record))
+
+
+class UniverseApplication(object):
+    """Encapsulates the state of a Universe application"""
+    def __init__(self, **kwargs):
+        print "Python version: ", sys.version_info[:2]
+        self.PERSISTENT_DATA_VERSION = 3  # Current expected and generated PD version
+        self.DEPLOYMENT_VERSION = 2
+        cc = CloudConfig(app=self)
+        # Get the type of cloud currently running on
+        self.cloud_type = cc.get_cloud_type()
+        # Create an appropriate cloud connection
+        self.cloud_interface = cc.get_cloud_interface(self.cloud_type)
+        # Load user data into a local field through a cloud interface
+        self.ud = self.cloud_interface.get_user_data()
+        # From user data determine if object store (S3) should be used.
+        self.use_object_store = self.ud.get("use_object_store", True)
+        # From user data determine if block storage (EBS/nova-volume) should be used.
+        # (OpenNebula and dummy clouds do not support volumes yet so skip those)
+        self.use_volumes = self.ud.get(
+            "use_volumes", self.cloud_type not in ['opennebula', 'dummy'])
+        # Read config file and check for errors
+        self.config = config.Configuration(**kwargs)
+        self.config.init_with_user_data(self.ud)
+        self.config.check()
+        # Setup logging
+        self.logger = CMLogHandler(self)
+        if "testflag" in self.ud:
+            self.TESTFLAG = bool(self.ud['testflag'])
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.TESTFLAG = False
+            self.logger.setLevel(logging.INFO)
+
+        if "localflag" in self.ud:
+            self.LOCALFLAG = bool(self.ud['localflag'])
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.LOCALFLAG = False
+            self.logger.setLevel(logging.INFO)
+        log.addHandler(self.logger)
+        config.configure_logging(self.config, self.ud)
+        log.debug("Initializing app")
+        log.debug("Running on '{0}' type of cloud in zone '{1}' using image '{2}'."
+                  .format(self.cloud_type, self.cloud_interface.get_zone(),
+                          self.cloud_interface.get_ami()))
+
+        # App-wide object to store messages that need to travel between the back-end
+        # and the UI.
+        # TODO: Ideally, this should be stored some form of more persistent
+        # medium (eg, database, file, session) and used as a simple module (vs. object)
+        # but that's hopefully still forthcoming.
+        self.msgs = messages.Messages()
+
+        # Check that we actually got user creds in user data and inform user
+        if not ('access_key' in self.ud or 'secret_key' in self.ud):
+            self.msgs.error("No access credentials provided in user data. "
+                            "You will not be able to add any services.")
+        # Update user data to include persistent data stored in cluster's bucket, if it exists
+        # This enables cluster configuration to be recovered on cluster re-
+        # instantiation
+        self.manager = None
+        if self.use_object_store and 'bucket_cluster' in self.ud:
+            log.debug("Getting pd.yaml")
+            if misc.get_file_from_bucket(self.cloud_interface.get_s3_connection(),
+               self.ud['bucket_cluster'], 'persistent_data.yaml', 'pd.yaml'):
+                pd = misc.load_yaml_file('pd.yaml')
+                self.ud = misc.merge_yaml_objects(self.ud, pd)
+                self.ud = misc.normalize_user_data(self, self.ud)
+            else:
+                log.debug("Setting deployment_version to {0}".format(self.DEPLOYMENT_VERSION))
+                # This is a new cluster so default to the current version
+                self.ud['deployment_version'] = self.DEPLOYMENT_VERSION
+
+    def startup(self):
+        if 'role' in self.ud:
+            if self.ud['role'] == 'master':
+                log.info("Master starting")
+                from cm.util import master
+                self.manager = master.ConsoleManager(self)
+            elif self.ud['role'] == 'worker':
+                log.info("Worker starting")
+                from cm.util import worker
+                self.manager = worker.ConsoleManager(self)
+            self.path_resolver = paths.PathResolver(self.manager)
+            self.manager.console_monitor.start()
+        else:
+            log.critical("************ No ROLE in %s - this is a fatal error. ************" % paths.USER_DATA_FILE)
+
+    def shutdown(self, delete_cluster=False):
+        if self.manager:
+            self.manager.shutdown(delete_cluster=delete_cluster)
---
+++ cm/services/apps/migration.py
@@ -0,0 +1,353 @@
+import os
+import pwd
+import grp
+import fnmatch
+from cm.services import ServiceRole
+from cm.services import service_states
+from cm.services import ServiceDependency
+from cm.services import ServiceType
+from cm.services.apps import ApplicationService
+from cm.util import misc, paths
+
+import logging
+log = logging.getLogger('cloudman')
+
+
+class Migrate1to2:
+    """Functionality for upgrading from version 1 to 2.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def _as_postgres(self, cmd, cwd=None):
+        return misc.run('%s - postgres -c "%s"' % (paths.P_SU, cmd), cwd=cwd)
+
+    def _upgrade_postgres_8_to_9(self):
+        old_data_dir = os.path.join(self.app.path_resolver.galaxy_data, "pgsql", "data")
+        new_data_dir = self.app.path_resolver.psql_dir
+        if old_data_dir == new_data_dir:
+            log.debug("Nothing to upgrade in database - paths are the same: %s" % old_data_dir)
+            return True
+
+        if os.path.exists(old_data_dir):
+            with open(os.path.join(old_data_dir, "PG_VERSION")) as in_handle:
+                version = in_handle.read().strip()
+            if version.startswith("8"):
+                log.debug("Upgrading Postgres from version 8 to 9")
+                old_debs = ["http://launchpadlibrarian.net/102366400/postgresql-client-8.4_8.4.11-1_amd64.deb",
+                            "http://launchpadlibrarian.net/102366396/postgresql-8.4_8.4.11-1_amd64.deb"]
+                for deb in old_debs:
+                    misc.run("wget %s" % deb)
+                    misc.run("dpkg -i %s" % os.path.basename(deb))
+                    misc.run("rm -f %s" % os.path.basename(deb))
+                backup_dir = os.path.join(self.app.path_resolver.galaxy_data, "pgsql",
+                                          "data-backup-v%s" % version)
+                self._as_postgres("mv %s %s" % (old_data_dir, backup_dir))
+                misc.run("mkdir -p %s" % new_data_dir)
+                os.chown(new_data_dir, pwd.getpwnam("postgres")[2], grp.getgrnam("postgres")[2])
+                self._as_postgres("%s/initdb %s" % (self.app.path_resolver.pg_home, new_data_dir))
+                self._as_postgres("sed -i 's|#port = 5432|port = {0}|' {1}"
+                                  .format(paths.C_PSQL_PORT, os.path.join(new_data_dir, 'postgresql.conf')))
+                # Seems to require a start and stop before the upgrade can work!
+                log_loc = os.path.join(new_data_dir, 'cm_postgres_upgrade.log')
+                self._as_postgres("{0}/pg_ctl -w -l {1} -D {2} start".
+                                  format(self.app.path_resolver.pg_home, log_loc, new_data_dir),
+                                  cwd=new_data_dir)
+                self._as_postgres("{0}/pg_ctl -w -l {1} -D {2} stop".
+                                  format(self.app.path_resolver.pg_home, log_loc, new_data_dir),
+                                  cwd=new_data_dir)
+                # Assume that if this step works, all the previous ones worked
+                ok = self._as_postgres("{0}/pg_upgrade -d {1} -D {2} -p 5840 -P {3} -b /usr/lib/postgresql/8.4/bin"
+                                       " -B {0} -l {4}".format(self.app.path_resolver.pg_home,
+                                                               backup_dir, new_data_dir,
+                                                               paths.C_PSQL_PORT, log_loc),
+                                       cwd=new_data_dir)
+                misc.run("apt-get -y --force-yes remove postgresql-8.4 postgresql-client-8.4")
+        return ok
+
+    def _upgrade_database(self):
+        return self._upgrade_postgres_8_to_9()
+
+    def _as_galaxy(self, cmd):
+        return misc.run('%s - galaxy -c "%s"' % (paths.P_SU, cmd))
+
+    def _copy_to_new_fs(self):
+        fs_galaxy_data = self.app.manager.get_services(svc_role=ServiceRole.GALAXY_DATA)[0]
+        fs_galaxy_tools = self.app.manager.get_services(svc_role=ServiceRole.GALAXY_TOOLS)[0]
+        source_path = os.path.join(fs_galaxy_tools.mount_point, "tools")
+        target_path = os.path.join(fs_galaxy_data.mount_point, "tools")
+        ok_tools = self._as_galaxy("cp -R {0} {1}".format(source_path, target_path))
+
+        source_path = os.path.join(fs_galaxy_tools.mount_point, "galaxy-central")
+        target_path = os.path.join(fs_galaxy_data.mount_point, "galaxy-app")
+        if (os.path.exists(target_path)):
+            log.debug("Target path for galaxy-app ({0}) already exists! Skipping...".format(target_path))
+            ok_galaxy = True
+        else:
+            ok_galaxy = self._as_galaxy("cp -R {0} {1}".format(source_path, target_path))
+        if (ok_tools and ok_galaxy):
+            return True
+        else:
+            log.error("Problems copying data to the new file system; look at the log.")
+            return False
+
+    def _create_missing_dirs(self):
+        # TODO: on the file system missing/renamig dirs: galaxyData/'files' will
+        # need to be renamed to galaxy/data'
+        return True
+
+    def _upgrade_fs_structure(self):
+        ok_fs = self._copy_to_new_fs()
+        ok_dirs = self._create_missing_dirs()
+        if (ok_fs and ok_dirs):
+            return True
+        else:
+            log.error("Problems upgrading file system structure; look at the log.")
+            return False
+
+    def _adjust_stored_paths(self):
+        # Adjust galaxy mercurial location for future updates
+        galaxy_data_loc = self.app.manager.get_services(svc_role=ServiceRole.GALAXY_DATA)[0].mount_point
+        galaxy_loc = os.path.join(galaxy_data_loc, "galaxy-app")
+        if os.path.isdir(galaxy_loc):
+            self._as_galaxy("sed -i 's/central/dist/' %s" % os.path.join(galaxy_loc, '.hg', 'hgrc'))
+        # Adjust tools location in galaxy
+        galaxy_ini_loc = os.path.join(galaxy_loc, 'universe_wsgi.ini')
+        ok_tools = self._as_galaxy("sed -i 's|tool_dependency_dir = /mnt/galaxyTools/tools|tool_dependency_dir = {0}|' {1}"
+                                   .format(os.path.join(galaxy_data_loc, 'tools'), galaxy_ini_loc))
+
+        ok_db = self._as_galaxy("sed -i 's|database_connection = postgres://galaxy@localhost:5840/galaxy|database_connection = postgres://galaxy@localhost:{0}/galaxy|' {1}"
+                                .format(paths.C_PSQL_PORT, galaxy_ini_loc))
+        # Adjust content of tools' env.sh to reflect the new path
+        tools_dir = os.path.join(galaxy_data_loc, 'tools')
+        env_files = []
+        for root, dirnames, filenames in os.walk(tools_dir):
+            for filename in fnmatch.filter(filenames, 'env.sh'):
+                env_files.append(os.path.join(root, filename))
+        log.debug("Found the following env.sh files: {0}".format(env_files))
+        for f in env_files:
+            cmd = "sed --in-place=.orig 's/galaxyTools/galaxyData/g' {0}".format(f)
+            misc.run(cmd)
+        # Update broken symlinks for the tools' `default` dirs
+        default_symlinks = misc.detect_symlinks('/mnt/galaxyData/tools',
+                                                'default',
+                                                symlink_as_file=False)
+        if not default_symlinks:
+            log.debug("No 'default' symlinks found to update!")
+        for link in default_symlinks:
+            link_name = link[0]
+            # We'll make the symlinks relative so extract target's basename
+            target_name = os.path.basename(link[1])
+            os.unlink(link_name)
+            os.symlink(target_name, link_name)
+            log.debug("Updated symlink {0} to {1}.".format(link_name, target_name))
+        if (ok_tools and ok_db):
+            return True
+        else:
+            log.error("Problems updating Galaxy tools or database location; look at the log.")
+            return False
+
+    def _update_user_data(self):
+        if 'filesystems' in self.app.ud:
+            old_fs_list = self.app.ud.get('filesystems') or []
+            new_fs_list = []
+            # clear 'services' and replace with the new format
+            for fs in old_fs_list:
+                svc_roles = ServiceRole.from_string_array(fs['roles'])
+                if ServiceRole.GALAXY_TOOLS in svc_roles and ServiceRole.GALAXY_DATA in svc_roles:
+                    # This condition should only occur in new configs, but check
+                    # added so that things work properly even if run against a new config
+                    # Only works for default configs though...
+                    new_fs_list.append(fs)
+                elif ServiceRole.GALAXY_TOOLS in svc_roles:
+                    pass  # skip adding the galaxy tools file system, no longer needed.
+                else:
+                    if ServiceRole.GALAXY_DATA in ServiceRole.from_string_array(fs['roles']):
+                        fs['roles'] = ServiceRole.to_string_array([ServiceRole.GALAXY_TOOLS,
+                                                                   ServiceRole.GALAXY_DATA])
+                        new_fs_list.append(fs)
+                    else:
+                        new_fs_list.append(fs)
+            self.app.ud['filesystems'] = new_fs_list
+        self.app.ud['deployment_version'] = 2
+        self.app.ud.pop('galaxy_home', None)  # TODO: Galaxy home is always reset to default. Discuss implications
+        return True
+
+    def _migrate1_prereqs_satisfied(self):
+        """
+        Make sure prerequisites for applying migration 1 to 2 are satisfied.
+        """
+        # 'Old' AMIs do not support this CloudMan version so guard against those
+        old_AMIs = ['ami-da58aab3', 'ami-9a7485f3', 'ami-46d4792f']
+        current_ami = self.app.cloud_interface.get_ami()
+        if current_ami in old_AMIs:
+            msg = ("The Machine Image you are running ({0}) does not support this version "
+                   "of CloudMan. You MUST terminate this instance and launch a new "
+                   "one using a more up-to-date Machine Image. See "
+                   "<a href='http://wiki.galaxyproject.org/CloudMan/' target='_blank'>"
+                   "this page</a> for more details.".format(current_ami))
+            log.critical(msg)
+            self.app.msgs.critical(msg)
+            return False
+        fs_galaxy_data_list = self.app.manager.get_services(svc_role=ServiceRole.GALAXY_DATA)
+        if not fs_galaxy_data_list:
+            log.warn("Required File system GALAXY_DATA missing. Aborting migrate1to2.")
+            return False
+        fs_galaxy_tools_list = self.app.manager.get_services(svc_role=ServiceRole.GALAXY_TOOLS)
+        if not fs_galaxy_tools_list:
+            log.warn("Required File system GALAXY_TOOLS missing. Aborting Aborting migrate1to2.")
+            return False
+        # Get a handle to the actual service objects
+        fs_galaxy_data = fs_galaxy_data_list[0]
+        fs_galaxy_tools = fs_galaxy_tools_list[0]
+        space_available = int(fs_galaxy_data.size) - int(fs_galaxy_data.size_used)
+        if space_available < int(fs_galaxy_tools.size_used):
+            log.debug("Cannot migrate from 1 to 2: Insufficient space available on "
+                      "Galaxy data volume. Available: {0}, Required: {1}"
+                      .format(space_available, fs_galaxy_tools.size_used))
+            return False
+        for svc in self.app.manager.get_services(svc_type=ServiceType.FILE_SYSTEM):
+            if ServiceRole.GALAXY_TOOLS in svc.svc_roles and ServiceRole.GALAXY_DATA in svc.svc_roles:
+                log.warn("File system appears to have been already migrated! Aborting.")
+                return False
+        return True
+
+    def migrate_1to2(self):
+        # First set the current ``deployment_version``, in case it's empty.
+        # This is to prevent it from being set to the latest version when empty
+        self.app.ud['deployment_version'] = 1
+
+        if not self._migrate1_prereqs_satisfied():
+            msg = "Cannot migrate from version 1 to 2. Pre-requisites not satisfied!"
+            log.warn(msg)
+            self.app.msgs.warning(msg)
+            return False
+
+        msg = ("Migrating this deployment from version 1 to 2. Note that this "
+               "may take a while. Please wait until the process completes before "
+               "starting to use any services or features.")
+        log.info(msg)
+        self.app.msgs.info(msg)
+        log.info("Migration: Step 1: Upgrading Postgres Database...")
+        ok = self._upgrade_database()
+        if ok:
+            log.info("Migration: Step 2: Upgrading to new file system structure...")
+            ok = self._upgrade_fs_structure()
+        if ok:
+            log.info("Migration: Step 3: Adjusting paths in configuration files...")
+            ok = self._adjust_stored_paths()
+        if ok:
+            log.info("Migration: Step 4: Updating user data...")
+            ok = self._update_user_data()
+        if ok:
+            log.info("Migration: Step 5: Shutting down all file system services...")
+            fs_svcs = self.app.manager.get_services(svc_type=ServiceType.FILE_SYSTEM)
+            # TODO: Is a clean necessary?
+            for svc in fs_svcs:
+                svc.remove(synchronous=True)
+            log.info("Migration: Step 6: Restarting all file system services...")
+            ok = self.app.manager.add_preconfigured_filesystems()
+        # Migration 1 to 2 complete
+        if ok:
+            msg = ("Migration from version 1 to 2 complete. Please continue to wait "
+                   "until all the services have completed initializing.")
+            log.info(msg)
+            self.app.msgs.info(msg)
+        return ok
+
+
+class MigrationService(ApplicationService, Migrate1to2):
+    def __init__(self, app):
+        super(MigrationService, self).__init__(app)
+
+        self.svc_roles = [ServiceRole.MIGRATION]
+        self.name = ServiceRole.to_string(ServiceRole.MIGRATION)
+
+        self.dependencies = []
+
+        if 'filesystems' in self.app.ud:
+            for fs in self.app.ud.get('filesystems') or []:
+                # Wait for galaxy data, indices and tools to come up before attempting migration
+                if ServiceRole.GALAXY_DATA in ServiceRole.from_string_array(fs['roles']):
+                    self.dependencies.append(ServiceDependency(self, ServiceRole.GALAXY_DATA))
+                if ServiceRole.GALAXY_TOOLS in ServiceRole.from_string_array(fs['roles']):
+                    self.dependencies.append(ServiceDependency(self, ServiceRole.GALAXY_TOOLS))
+                if ServiceRole.GALAXY_INDICES in ServiceRole.from_string_array(fs['roles']):
+                    self.dependencies.append(ServiceDependency(self, ServiceRole.GALAXY_INDICES))
+
+    def start(self):
+        """
+        Start the migration service
+        """
+        log.debug("Starting migration service...")
+        self.state = service_states.STARTING
+        if self._is_migration_needed():
+            self.state = service_states.RUNNING
+            try:
+                if not self._apply():
+                    msg = "Error running the migration service! Look at the log for clues."
+                    log.error(msg)
+                    self.app.msgs.error(msg)
+                    self.state = service_states.ERROR
+            except Exception, e:
+                log.error("Error starting migration service: {0}".format(e))
+                self.state = service_states.ERROR
+            else:
+                self.state = service_states.COMPLETED
+        else:
+            log.debug("No migration required. Service complete.")
+            self.state = service_states.COMPLETED
+
+    def _apply(self):
+        """
+        Apply the migration; do the actual work.
+        """
+        log.debug("Migration is required. Starting...")
+        return self._perform_migration()
+
+    def _perform_migration(self):
+        """
+        Based on the version number, carry out appropriate migration actions
+        """
+        if self._get_old_version() <= 1:
+            return self.migrate_1to2()
+        # No migration necessary so all is good
+        return True
+
+    def _is_migration_needed(self):
+        return self._get_old_version() < self._get_current_version()
+
+    def _get_current_version(self):
+        version = 2
+        log.debug("Current deployment version: {0}".format(version))
+        return version  # Whichever version that this upgrade script last understands
+
+    def _get_old_version(self):
+        # TODO: Need old version discovery. Where do we get that from?
+        version = self.app.ud.get('deployment_version', None)
+        if not version:
+            version = 1  # A version prior to version number being introduced
+        log.debug("Old deployment version: {0}".format(version))
+        return version
+
+    def remove(self, synchronous=False):
+        """
+        Remove the migration service
+        """
+        log.info("Removing Migration service")
+        super(MigrationService, self).remove(synchronous)
+        self.state = service_states.SHUTTING_DOWN
+        self._clean()
+        self.state = service_states.SHUT_DOWN
+
+    def _clean(self):
+        """
+        Clean up the system
+        """
+        pass
+
+    def status(self):
+        """
+        Check and update the status of service
+        """
+        pass
