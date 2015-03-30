7daeacd3d7bb06925424b2207bea71873d03c8cc
Enis Afgan <afgane@gmail.com>
2015-03-30 14:18:36 -0400

    Some changes to how Galaxy service state is managed to make it possible to start on builder instances w/o FS dependencies

--- cm/services/apps/galaxy.py
+++ cm/services/apps/galaxy.py
@@ -10,7 +10,6 @@ from cm.services import ServiceDependency
 from cm.util import paths
 from cm.util import misc
 from cm.util.decorators import TestFlag, delay
-from cm.util.galaxy_conf import attempt_chown_galaxy
 from cm.util.galaxy_conf import galaxy_option_manager
 from cm.util.galaxy_conf import populate_process_options
 from cm.util.galaxy_conf import populate_dynamic_options
@@ -62,16 +61,8 @@ class GalaxyService(ApplicationService):
     def remove(self, synchronous=False):
         log.info("Removing '%s' service" % self.name)
         super(GalaxyService, self).remove(synchronous)
-        if self.state == service_states.RUNNING or \
-           self.state == service_states.ERROR:
-            self.state = service_states.SHUTTING_DOWN
-            self.last_state_change_time = datetime.utcnow()
-            self.manage_galaxy(False)
-        elif self.state == service_states.UNSTARTED:
-            self.state = service_states.SHUT_DOWN
-        else:
-            log.debug("Galaxy service not running (state: {0}) so not stopping it."
-                      .format(self.state))
+        self.state = service_states.SHUTTING_DOWN
+        self.manage_galaxy(False)
 
     def restart(self):
         log.info('Restarting Galaxy service')
@@ -81,6 +72,15 @@ class GalaxyService(ApplicationService):
 
     @TestFlag(None)
     def manage_galaxy(self, to_be_started=True):
+        """
+        Use this method to start and stop Galaxy application.
+
+        :type to_be_started: bool
+        :param to_be_started: If set, this method will attempt to start the
+                              Galaxy application process. If not set, the
+                              method will attempt to shut down the application
+                              process.
+        """
         log.debug("Using Galaxy from '{0}'".format(self.galaxy_home))
         os.putenv("GALAXY_HOME", self.galaxy_home)
         os.putenv("TEMP", self.app.path_resolver.galaxy_temp)
@@ -112,98 +112,35 @@ class GalaxyService(ApplicationService):
             self.status()
             if not self.configured:
                 log.debug("Setting up Galaxy application")
+                # Set job manager configs if necessary
                 for job_manager_svc in self.app.manager.service_registry.active(
                         service_role=ServiceRole.JOB_MANAGER):
                     if ServiceRole.SGE in job_manager_svc.svc_roles:
                         log.debug("Running on SGE; setting env_vars")
                         self.env_vars["SGE_ROOT"] = self.app.path_resolver.sge_root,
                         self.env_vars["DRMAA_LIBRARY_PATH"] = self.app.path_resolver.drmaa_library_path
-                # s3_conn = self.app.cloud_interface.get_s3_connection()
+                # Make sure Galaxy home dir exists
                 if not os.path.exists(self.galaxy_home):
-                    log.error("Galaxy application directory '%s' does not exist! Aborting." %
-                              self.galaxy_home)
+                    log.error("Galaxy application directory '%s' does not "
+                              "exist! Aborting." % self.galaxy_home)
                     log.debug("ls /mnt/: %s" % os.listdir('/mnt/'))
                     self.state = service_states.ERROR
                     self.last_state_change_time = datetime.utcnow()
                     return False
-                # If a configuration file is not already in Galaxy's dir,
-                # retrieve it from a persistent data repository (i.e., S3)
-                # if s3_conn:
-                #     for f_name in ['universe_wsgi.ini',
-                #                    'tool_conf.xml',
-                #                    'tool_data_table_conf.xml',
-                #                    'shed_tool_conf.xml',
-                #                    'datatypes_conf.xml',
-                #                    'shed_tool_data_table_conf.xml']:
-                #         f_path = os.path.join(self.galaxy_home, f_name)
-                #         if not os.path.exists(f_path):
-                #             if not misc.get_file_from_bucket(s3_conn, self.app.ud['bucket_cluster'],
-                #                     '{0}.cloud'.format(f_name), f_path):
-                #                 # We did not get the config file from cluster's
-                #                 # bucket so get it from the default bucket
-                #                 log.debug("Did not get Galaxy configuration file " +
-                #                           "'{0}' from cluster bucket '{1}'"
-                #                           .format(f_name, self.app.ud['bucket_cluster']))
-                #                 log.debug("Trying to retrieve one ({0}.cloud) "
-                #                           "from the default '{1}' bucket."
-                #                           .format(f_name, self.app.ud['bucket_default']))
-                #                 local_file = os.path.join(self.galaxy_home, f_name)
-                #                 misc.get_file_from_bucket(s3_conn,
-                #                     self.app.ud['bucket_default'], '{0}.cloud'.format(f_name),
-                #                     local_file)
-                #                 attempt_chown_galaxy_if_exists(local_file)
-
-                # Make sure the temporary job_working_directory exists on user
-                # data volume (defined in universe_wsgi.ini.cloud)
-                if not os.path.exists('%s/tmp/job_working_directory' % self.app.path_resolver.galaxy_data):
-                    os.makedirs('%s/tmp/job_working_directory/' % self.app.path_resolver.galaxy_data)
-                attempt_chown_galaxy('%s/tmp/' % self.app.path_resolver.galaxy_data,
-                                     recursive=True)
-                # Make sure the default shed_tools directory exists
-                if not os.path.exists('%s/../shed_tools' % self.app.path_resolver.galaxy_data):
-                    os.makedirs('%s/../shed_tools/' % self.app.path_resolver.galaxy_data)
-                attempt_chown_galaxy('%s/../shed_tools/' % self.app.path_resolver.galaxy_data)
-                # TEMPORARY ONLY - UNTIL SAMTOOLS WRAPPER IS CONVERTED TO USE
-                # DATA TABLES
-                # if os.path.exists('/mnt/galaxyIndices/locfiles/sam_fa_indices.loc'):
-                #     shutil.copy(
-                #         '/mnt/galaxyIndices/locfiles/sam_fa_indices.loc',
-                #         '%s/tool-data/sam_fa_indices.loc' % self.galaxy_home)
-                # Ensure the environment is setup for running Galaxy
-                # This can also be setup on the tools snapshot and thus avoid these patches
-                # try:
-                #     subprocess.call( "sed 's/cd `dirname $0`/cd `dirname $0`; export TEMP=\/mnt\/galaxyData\/tmp/; export DRMAA_LIBRARY_PATH=/opt/sge/lib/lx24-amd64/libdrmaa.so.1.0' %s/run.sh > %s/run.sh.custom" % (self.galaxy_home, self.galaxy_home), shell=True )
-                #     misc.run("cd %s; sed 's/python/python -ES/g' run.sh.custom > run.sh" % self.galaxy_home, "Failed to adjust run.sh", "Successfully adjusted run.sh")
-                #     shutil.copy( self.galaxy_home + '/run.sh.custom', self.galaxy_home + '/run.sh' )
-                #     os.chown( self.galaxy_home + '/run.sh', pwd.getpwnam( "galaxy" )[2], grp.getgrnam( "galaxy" )[2] )
-                # except Exception, e:
-                #     log.debug("Problem customizing Galaxy's run.sh: %s" % e)
-                # try:
-                #     misc.run("cd %s; sed 's/pyhton/python -ES/g' setup.sh > setup.sh.custom" % self.galaxy_home, "Failed to edit setup.sh", "Successfully adjusted setup.sh")
-                #     shutil.copy( self.galaxy_home + '/setup.sh.custom', self.galaxy_home + '/setup.sh' )
-                #     os.chown( self.galaxy_home + '/setup.sh', pwd.getpwnam( "galaxy" )[2], grp.getgrnam( "galaxy" )[2] )
-                # except Exception, e:
-                #     log.error("Error adjusting setup.sh: %s" % e)
-                # subprocess.call( 'sed "s/#start_job_runners = pbs/start_job_runners = sge/" $GALAXY_HOME/universe_wsgi.ini > $GALAXY_HOME/universe_wsgi.ini.custom', shell=True )
-                # shutil.move( self.galaxy_home + '/universe_wsgi.ini.custom', self.galaxy_home + '/universe_wsgi.ini' )
-                # subprocess.call( 'sed "s/#default_cluster_job_runner = pbs:\/\/\//default_cluster_job_runner = sge:\/\/\//" $GALAXY_HOME/universe_wsgi.ini > $GALAXY_HOME/universe_wsgi.ini.custom', shell=True )
-                # shutil.move( self.galaxy_home + '/universe_wsgi.ini.custom', self.galaxy_home + '/universe_wsgi.ini' )
-                # Configure PATH in /etc/profile because otherwise some tools do not work
-                # with open('/etc/profile', 'a') as f:
-                #     f.write('export PATH=/mnt/galaxyTools/tools/bin:/mnt/galaxyTools/tools/pkg/fastx_toolkit_0.0.13:/mnt/galaxyTools/tools/pkg/bowtie-0.12.5:/mnt/galaxyTools/tools/pkg/samtools-0.1.7_x86_64-linux:/mnt/galaxyTools/tools/pkg/gnuplot-4.4.0/bin:/opt/PostgreSQL/8.4/bin:$PATH\n')
-                # os.chown(self.galaxy_home + '/universe_wsgi.ini',
-                # pwd.getpwnam("galaxy")[2], grp.getgrnam("galaxy")[2])
+                # Ensure the necessary directories exist
+                for dir_name in [paths.P_GALAXY_INDICES,
+                                 ('%s/tmp/job_working_directory' %
+                                  self.app.path_resolver.galaxy_data)]:
+                    misc.make_dir(dir_name, 'galaxy')
                 self.remaining_start_attempts -= 1
                 self.configured = True
-            if self.state != service_states.RUNNING:
+            if not self._is_galaxy_running():
                 log.debug("Starting Galaxy...")
                 # Make sure admin users get added
                 self.update_galaxy_config()
                 start_command = self.galaxy_run_command(
                     "%s --daemon" % self.extra_daemon_args)
-                log.debug(start_command)
-                if not misc.run(start_command, "Error invoking Galaxy",
-                        "Successfully initiated Galaxy start from {0}.".format(self.galaxy_home)):
+                if not misc.run(start_command):
                     if self.remaining_start_attempts > 0:
                         self.state = service_states.UNSTARTED
                         self.last_state_change_time = datetime.utcnow()
@@ -216,7 +153,10 @@ class GalaxyService(ApplicationService):
             log.info("Shutting down Galaxy...")
             self.state = service_states.SHUTTING_DOWN
             stop_command = self.galaxy_run_command("%s --stop-daemon" % self.extra_daemon_args)
-            if misc.run(stop_command):
+            if self._is_galaxy_running():
+                misc.run(stop_command)
+            if not self._is_galaxy_running():
+                log.debug("Galaxy not running; setting service state to SHUT_DOWN.")
                 self.state = service_states.SHUT_DOWN
                 self.last_state_change_time = datetime.utcnow()
                 # Move all log files
@@ -227,6 +167,16 @@ class GalaxyService(ApplicationService):
         return self.app.config.multiple_processes
 
     def galaxy_run_command(self, args):
+        """
+        Compose the command used to manage Galaxy process.
+
+        This will source Galaxy's virtualenv and compose the run command
+        with provided `args`.
+
+        :type args: string
+        :param args: Arguments to feed to Galaxy's run command, for example:
+                     `--daemon` or `--stop-daemon`.
+        """
         env_exports = "; ".join(["export %s='%s'" % (
             key, value) for key, value in self.env_vars.iteritems()])
         venv = "source $GALAXY_HOME/.venv/bin/activate"
@@ -236,18 +186,10 @@ class GalaxyService(ApplicationService):
 
     @delay
     def status(self):
-        """Check if Galaxy daemon is running and the UI is accessible."""
+        """Set the status of the service based on the state of the app process."""
         old_state = self.state
-        if self._check_daemon('galaxy'):
-            # log.debug("Galaxy daemon running. Checking if UI is accessible.")
-            if self._is_galaxy_running():
-                self.state = service_states.RUNNING
-            else:
-                log.debug("Galaxy UI does not seem to be accessible.")
-                self.state = service_states.STARTING
-        elif self.state == service_states.STARTING:
-            # Start process failed
-            self.state = service_states.ERROR
+        if self._is_galaxy_running():
+            self.state = service_states.RUNNING
         elif (self.state == service_states.SHUTTING_DOWN or
               self.state == service_states.SHUT_DOWN or
               self.state == service_states.UNSTARTED or
@@ -290,14 +232,19 @@ class GalaxyService(ApplicationService):
             self.app.manager.console_monitor.store_cluster_config()
 
     def _is_galaxy_running(self):
-        dns = "http://127.0.0.1:8080"
-        running_error_codes = [403]  # Error codes that indicate Galaxy is running
-        try:
-            urllib2.urlopen(dns)
-            return True
-        except urllib2.HTTPError, e:
-            return e.code in running_error_codes
-        except:
+        """Check is Galaxy process is running and the UI is accessible."""
+        if self._check_daemon('galaxy'):
+            dns = "http://127.0.0.1:8080"
+            running_error_codes = [403]  # Error codes that indicate Galaxy is running
+            try:
+                urllib2.urlopen(dns)
+                return True
+            except urllib2.HTTPError, e:
+                return e.code in running_error_codes
+            except:
+                return False
+        else:
+            log.debug("Galaxy UI does not seem to be accessible.")
             return False
 
     def update_galaxy_config(self):
