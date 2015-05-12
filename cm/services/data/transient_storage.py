603dec0f0b328bdf34716850b4bce6af5e3e4cdb
Enis Afgan <afgane@gmail.com>
2015-05-12 16:10:02 -0400

    Remove method that mounted missing ephemeral disk (it's done in ec2autorun.py instead (if not, this method would mount the new FS over existing data): https://github.com/galaxyproject/ansible-cloudman-image/commit/0566b51ae61379fcf3f56a788814fa66f17eb382)

--- cm/services/data/transient_storage.py
+++ cm/services/data/transient_storage.py
@@ -54,9 +54,6 @@ class TransientStorage(BlockStorage):
         and exporting it over NFS. Set the owner of the repo as ``ubuntu`` user.
         """
         log.debug("Adding a transient FS at {0}".format(self.fs.mount_point))
-        # transient_nfs gets created first - make sure it's on a dedicated device
-        if self.fs.name == 'transient_nfs' and self.app.cloud_type == 'ec2':
-            self._ensure_ephemeral_disk_mounted()
         misc.make_dir(self.fs.mount_point, owner='ubuntu')
         # Set the device ID
         cmd = "df %s | grep -v Filesystem | awk '{print $1}'" % self.fs.mount_point
@@ -84,25 +81,6 @@ class TransientStorage(BlockStorage):
         log.debug("Removing transient file system {0}".format(self.fs.mount_point))
         self.fs.remove_nfs_share()
         self.fs.state = service_states.SHUT_DOWN
-
-    def _ensure_ephemeral_disk_mounted(self):
-        """
-        Make sure `/mnt` is a mounted device vs. just being part of `/`.
-
-        At least some AWS instance types (e.g., r3) do not auto-mount what's in
-        `/ets/fstab` so make sure the ephemeral disks are in fact mounted.
-        http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/InstanceStorage.html#InstanceStoreTrimSupport
-        """
-        if not misc.run('mountpoint -q /mnt'):
-            device = '/dev/xvdb'  # Most of AWS instances have this device
-            if os.path.exists(device):
-                log.debug("/mnt is not a mountpoint; will try to mount it from {0}"
-                          .format(device))
-                misc.run('mkfs.xfs {0}'.format(device))
-                misc.run('mount -o discard {0} /mnt'.format(device))
-            else:
-                log.warning("Mountpoint /mnt not available and no device {0}"
-                            .format(device))
 
     def status(self):
         """
