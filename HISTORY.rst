e40ea5d65df02b551c225139e0f2b4d823dde8de
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-05-16 16:38:40 +0530

    Added setup.py for pip installation

---
+++ HISTORY.rst
@@ -0,0 +1,9 @@
+.. :changelog:
+
+History
+-------
+
+2.0.0 (2018-05-16)
+++++++++++++++++++
+
+* Development version of CloudManV2
---
+++ cloudman/cloudman/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# Current version of the library
+__version__ = '2.0.0'
+
+
+def get_version():
+    """
+    Return a string with the current version of the library.
+
+    :rtype: ``string``
+    :return:  Library version (e.g., "2.0.0").
+    """
+    return __version__
