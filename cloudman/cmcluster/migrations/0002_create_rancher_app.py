b1b31ada038f586796ad3c5b33a9c8d132fe9cb4
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-04-08 00:37:31 +0530

    Improved initial data migration script

--- cloudman/cmcluster/migrations/0002_create_rancher_app.py
+++ cloudman/cmcluster/migrations/0002_create_rancher_app.py
@@ -2,21 +2,36 @@
 import os
 from django.db import migrations
 from django.core import serializers
+from django.core.management import call_command
 
 
-# based on: https://stackoverflow.com/questions/32912112/django-loaddata-in-
-# migrations-errors
-def create_rancher_app(apps, schema_editor):
-    original_apps = serializers.python.apps
-    serializers.python.apps = apps
-    fixture_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                '../fixtures/rancher_app_def.json')
-    fixture = open(fixture_file)
-    objects = serializers.deserialize('json', fixture, ignorenonexistent=True)
-    for obj in objects:
-        obj.save()
-    fixture.close()
-    serializers.python.apps = original_apps
+# based on: https://stackoverflow.com/a/39743581/10971151
+def import_data(apps, schema_editor, filename):
+    # Save the old _get_model() function
+    old_get_model = serializers.python._get_model
+
+    # Define new _get_model() function here, which utilizes the apps argument
+    # to get the historical version of a model.
+    def _get_model(model_identifier):
+        try:
+            return apps.get_model(model_identifier)
+        except (LookupError, TypeError):
+            raise serializers.base.DeserializationError(
+                "Invalid model identifier: '%s'" % model_identifier)
+
+    # Replace the _get_model() function, so loaddata can utilize it.
+    serializers.python._get_model = _get_model
+
+    try:
+        # Call loaddata command
+        call_command('loaddata', filename, app_label='cmcluster')
+    finally:
+        # Restore old _get_model() function
+        serializers.python._get_model = old_get_model
+
+
+def import_rancher_app(apps, schema_editor):
+    import_data(apps, schema_editor, 'rancher_app_def.json')
 
 
 class Migration(migrations.Migration):
@@ -26,5 +41,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_rancher_app)
+        migrations.RunPython(import_rancher_app)
     ]
