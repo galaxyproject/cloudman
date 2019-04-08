85401efcf24162c12f7f4de31fdd2afacf124249
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-04-09 00:00:39 +0530

    Remove unused code

--- local_dev.sh
+++ local_dev.sh
@@ -1,10 +1,11 @@
 . venv/bin/activate
-CONSUL_PATH="./venv/bin"
-if [ ! -f $CONSUL_PATH/consul ]; then
-   echo "Extracting consul to: $CONSUL_PATH"
-   wget -O- https://releases.hashicorp.com/consul/0.9.3/consul_0.9.3_darwin_amd64.zip | tar xvz -C $CONSUL_PATH
-fi
-$CONSUL_PATH/consul agent -dev &
+#CONSUL_PATH="./venv/bin"
+#if [ ! -f $CONSUL_PATH/consul ]; then
+#   echo "Extracting consul to: $CONSUL_PATH"
+#   wget -O- https://releases.hashicorp.com/consul/0.9.3/consul_0.9.3_darwin_amd64.zip | tar xvz -C $CONSUL_PATH
+#fi
+#$CONSUL_PATH/consul agent -dev &
+export CELERY_BROKER_URL="amqp://guest:guest@localhost:5672/"
 cd cloudman
 /usr/local/Cellar/rabbitmq/3.6.9/sbin/rabbitmq-server &
 celery -E -A cloudman worker -l debug &
