c9fc78bb1b27099b8245efae02b3e84419741559
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-04-19 23:30:48 +0530

    Fixed rancher node command

--- cloudman/cmcluster/plugins/rancher_kubernetes_app.py
+++ cloudman/cmcluster/plugins/rancher_kubernetes_app.py
@@ -71,7 +71,7 @@ class RancherKubernetesAnsibleAppConfigurer(AnsibleAppConfigurer):
 
     def configure(self, app_config, provider_config):
         playbook_vars = [
-            ('rancher_node_command', app_config.get('rancher_config', {}).get(
+            ('rancher_node_command', app_config.get('config_rancher_kube', {}).get(
                 'rancher_node_command'))
         ]
         return super().configure(app_config, provider_config,
