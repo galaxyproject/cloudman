658d0d1ff3bc85db9603f655a085b6c1be009214
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-02-09 17:35:48 +0530

    Setup in-memory celery with eager eval for tests

---
+++ cloudman/cloudman/celeryconfig_test.py
@@ -0,0 +1,11 @@
+"""
+Celery settings used during cloudlaunch testing
+"""
+broker_url = 'memory://'
+broker_transport_options = {'polling_interval': .01}
+broker_backend = 'memory'
+result_backend = 'db+sqlite:///results.db'
+result_serializer = 'json'
+task_serializer = 'json'
+accept_content = ['json']
+task_always_eager = True
