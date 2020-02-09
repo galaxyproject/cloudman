652b85ab466a27f571fbd0af34c757e27c7f4128
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-02-09 17:37:07 +0530

    Add missing helmsman rule file

---
+++ cloudman/helmsman/rules.py
@@ -0,0 +1,14 @@
+import rules
+
+# Delegate to keycloak in future iteration
+
+# Permissions
+rules.add_perm('helmsman.view_namespace', rules.is_staff)
+rules.add_perm('helmsman.add_namespace', rules.is_staff)
+rules.add_perm('helmsman.change_namespace', rules.is_staff)
+rules.add_perm('helmsman.delete_namespace', rules.is_staff)
+
+rules.add_perm('helmsman.view_chart', rules.is_staff)
+rules.add_perm('helmsman.add_chart', rules.is_staff)
+rules.add_perm('helmsman.change_chart', rules.is_staff)
+rules.add_perm('helmsman.delete_chart', rules.is_staff)
